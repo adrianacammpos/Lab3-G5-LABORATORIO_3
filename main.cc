7175537ecaa2e86cc4eee0c86b9289b8956eae37
DiegoF24 <00233620@uca.edu.sv>
2024-09-03 10:37:01 -0600

    agregar funcion de potencia

--- main.cc
+++ main.cc
@@ -1,6 +1,30 @@
 #include <iostream>
-int main(int argc, char *argv[])
+
+int Potencia(int, int);
+
+int Potencia(int a, int b)
 {
-    
+    // Caso base: cualquier número elevado a la potencia 0 es 1
+    if (b == 0)
+        return 1;
+    // Caso recursivo: multiplicar la base por la potencia de la base al exponente menos 1
+    else
+        return a * Potencia(a, b - 1);
+}
+
+int main()
+{
+    int base, exponente;
+
+    std::cout << "Ingrese la base: ";
+    std::cin >> base;
+
+    std::cout << "Ingrese el exponente: ";
+    std::cin >> exponente;
+
+    int result = Potencia(base, exponente);
+
+    std::cout << base << " elevado a la " << exponente << " es: " << result << std::endl;
+
     return 0;
 }
